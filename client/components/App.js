7911dd4d8fea01fdaf72c3b03492a32ef5e2940f
Katerina Solovyova <yekaterina.solovyova@gmail.com>
2021-08-10 13:57:28 -0400

    added in a sample view of first 100 PRs.

--- client/components/App.js
+++ client/components/App.js
@@ -3,13 +3,15 @@ import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import { Button } from '@material-ui/core';
+import PRsContainer from './PRsContainer'
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
 			prState: '',
-			prCount: ''
+			prCount: '',
+			prs: []
 		}
 		this.setStateOfPR = this.setStateOfPR.bind(this)
 		this.generateCount = this.generateCount.bind(this)
@@ -32,21 +34,25 @@ export default class App extends React.Component {
 	async getResults(prState) {
 		let url
 		if (prState === '') {
-			url = `https://api.github.com/search/issues?q=org:ramda+is:pr`
+			url = `https://api.github.com/search/issues?q=org:ramda+is:pr&per_page=100`
 		}
 		else {
-			url = `https://api.github.com/search/issues?q=org:ramda+is:pr+state:${this.state.prState}`
+			url = `https://api.github.com/search/issues?q=org:ramda+is:pr+state:${this.state.prState}&per_page=100`
 		}
 		// let response = await axios.get(url)
 		let response = await fetch(url)
-		console.log('RESPONSE')
-		console.log(response)
-		let result = await response.json()
-		console.log('RESULT')
-		console.log(result)
-		console.log('COUNT')
-		console.log(result.total_count)
-		return result
+		// console.log('RESPONSE')
+		// console.log(response)
+		let results = await response.json()
+		// console.log('RESULT')
+		// console.log(results)
+		// console.log('FIRST RESULT DATE IS')
+		// console.log(results.items[0].created_at)
+		// console.log('NOW WITH TEXT TRIMMED')
+		// console.log(results.items[0].created_at.substring(0,10))
+		// console.log('COUNT')
+		// console.log(results.total_count)
+		return results
 	}
 
 	async generateCount (e) {
@@ -55,14 +61,16 @@ export default class App extends React.Component {
 		let {prState} = this.state
 		let finalResults = await this.getResults(prState)
 		this.setState({
-			prCount: finalResults.total_count
+			prCount: finalResults.total_count,
+			prs: finalResults.items
 		})
-		console.log('COUNT IS NOW IN STATE: ', this.state.prCount)
+		// console.log('COUNT IS NOW IN STATE: ', this.state.prCount)
+		// console.log('RESULTS ARE NOW IN STATE: ', this.state.prs)
 	}
 	render () {
 	return (
 		<div id = "landing">
-			<h1>Ramda Pull Requests</h1>
+			<h1>Ramda Organization Pull Requests</h1>
 			<Grid>
 				<TextField
 					select label="State of PR (Default: All)"
@@ -77,10 +85,27 @@ export default class App extends React.Component {
 					<MenuItem value="closed">Closed</MenuItem>
 				</TextField>
 			</Grid>
+			<br></br>
 			<Button className="generatePRCountButton" variant="contained" color="secondary" onClick={this.generateCount}>Generate PR Count</Button>
 			<h2>Total Count: {this.state.prCount}</h2>
-
+			<div id="prsSample">
+				<h4>Sample PRs (first 100 results)</h4>
+				{/* <PRsContainer /> */}
+				<div id="prsContainer">
+					{this.state.prs.map(pr => {return (
+					<div id="singlePR" key={pr.id}>
+						<b>{pr.title}</b>
+						<br />
+						Created at: {pr.created_at.substring(0,10)}
+						<br />
+						<i>- {pr.user.login}</i>
+						<p/>
+					</div>
+					)})}
+				</div>
+			</div>
 		</div>
+
 	)
 	}
 }
